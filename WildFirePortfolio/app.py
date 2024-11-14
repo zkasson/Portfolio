bc133ab4b408f3125d018af32af5b1912e87a888
zkasson <121266165+zkasson@users.noreply.github.com>
2024-11-14 13:07:09 -0800

    Update app.py

--- WildFirePortfolio/app.py
+++ WildFirePortfolio/app.py
@@ -5,7 +5,13 @@ import matplotlib.pyplot as plt
 import leafmap.foliumap as leafmap
 import arcgis
 import gssapi
+gis = GIS()
 
+# Set up 
+st.set_page_config(page_title='Dashboard', layout='wide')
+st.title('Canadian Wildfire Dashboard')
+st.sidebar.title('About')
+st.sidebar.info('Explore Active Wildfire in Canada')
 
 
 
