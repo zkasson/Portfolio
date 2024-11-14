06b139772593e41c362d2ae10dadcc31e65b3bbb
zkasson <121266165+zkasson@users.noreply.github.com>
2024-11-14 13:10:55 -0800

    Create wildfireApp.py

---
+++ wildfireApp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import leafmap.foliumap as leafmap
+from arcgis.gis import GIS
+from arcgis import GeoAccessor
+gis = GIS()
+
+# Set up 
+st.set_page_config(page_title='Dashboard', layout='wide')
+st.title('Canadian Wildfire Dashboard')
+st.sidebar.title('About')
+st.sidebar.info('Explore Active Wildfire in Canada')
