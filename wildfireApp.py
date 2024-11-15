8dbcd98e537d260aae5f8300e77f1f401c541340
zkasson <121266165+zkasson@users.noreply.github.com>
2024-11-15 12:42:09 -0800

    Update wildfireApp.py

--- wildfireApp.py
+++ wildfireApp.py
@@ -7,6 +7,7 @@ from arcgis.gis import GIS
 from arcgis.features import GeoAccessor, GeoSeriesAccessor
 gis = GIS()
 
+
 # Set up 
 st.set_page_config(page_title='Dashboard', layout='wide')
 st.title('Canadian Wildfire Dashboard')
@@ -31,24 +32,37 @@ agency_to_province = {
     'yt': 'Yukon'
 }
 
+
 @st.cache_data
 def read_fl(item_id):
     living_atlas_item = gis.content.get(item_id)
     feature_layer = living_atlas_item.layers[0]
-    wf_sdf = feature_layer.query(where="1=1", out_sr=4326).sdf
-    return wf_sdf
+    sdf = feature_layer.query(where="1=1", out_sr=4326).sdf
+    return sdf
+@st.cache_data
+def read_json(url):
+    territories_gdf = gpd.read_file(url)
+    # Clean the 'prov_name_en' column to remove brackets and rename it
+    if 'prov_name_en' in territories_gdf.columns:
+        territories_gdf['prov_name_en'] = territories_gdf['prov_name_en'].apply(lambda x: x[0] if isinstance(x, list) else x)
+        territories_gdf.rename(columns={'prov_name_en': 'English Name'}, inplace=True)
+    return territories_gdf
 
-# Retrieve Wildfire layer and create SDF
+
+
+# Retrieve Wildfire layer and create SDF & Retrieve territories layer and create SDF
 item_id = "21638fcd54d14a25b6f1affdef812146"
+json_file = 'https://raw.githubusercontent.com/zkasson/Portfolio/refs/heads/main/georef-canada-province%40public.geojson'
 wildfire_sdf = read_fl(item_id)
+territories_gdf = read_json(json_file)
 
-# Filter and create Province column
+
+# Filter and create Province column, Map from agency to province
 conus_fires = wildfire_sdf[wildfire_sdf['Agency'] == 'conus']
 canada_wildfire_sdf = wildfire_sdf[wildfire_sdf['Agency'] != 'conus']
 canada_wildfire_sdf['Province'] = wildfire_sdf['Agency'].map(agency_to_province)
 canada_wildfire_sdf = canada_wildfire_sdf.drop(columns=['Agency'])
 
-# # # Create chart in side bar # # #
 
 # Create dropdown for provinces
 provinces = canada_wildfire_sdf['Province'].unique()
@@ -72,12 +86,46 @@ stage_of_control_mapping = {
 canada_wildfire_sdf['Stage_of_Control'] = canada_wildfire_sdf['Stage_of_Control'].replace(stage_of_control_mapping)
 
 
-# Filter for fires in specific provinces
+# Filter for fires in specific provinces -- This is for Spatial use 
 filtered_fires = canada_wildfire_sdf[canada_wildfire_sdf['Province'] == province] 
 
-st.write(filtered_fires)
+
+# Group by Province and Stage_of_Control, then sum Hectares__Ha_
+area_sdf = canada_wildfire_sdf.groupby(['Province', 'Stage_of_Control'])['Hectares__Ha_'].sum().reset_index()
+area_sdf = area_sdf.pivot(index='Province', columns='Stage_of_Control', values='Hectares__Ha_').fillna(0)
+area_sdf.columns.name = None
+
 
 unit = st.sidebar.radio(
     "Select a Unit",
     ['Hectares', 'Acres']
 )
+
+conversion_factor = 2.47105
+def correct_unit(area_sdf,unit_df):
+    if unit_df == 'Hectares':  
+        area_final = area_sdf 
+        return area_final
+    elif unit_df == 'Acres':
+        area_final = area_sdf
+        area_final['Prescribed'] = area_sdf['Prescribed'] * conversion_factor
+        area_final['Being Held'] = area_sdf['Being Held'] * conversion_factor
+        area_final['Out of Control'] = area_sdf['Out of Control'] * conversion_factor
+        area_final['Under Control'] = area_sdf['Under Control'] * conversion_factor
+        return area_final
+area_final = correct_unit(area_sdf,unit)
+
+# Calculate the upper limit for the y-axis
+max_sh = area_final['Under Control'].max()  # Find the max value in the SH column
+upper_limit = max_sh + 2500  
+rounded_upper_limit = round(upper_limit / 100) * 100 
+
+# Create plot
+fig, ax = plt.subplots(1, 1)
+
+area_final.plot(kind='bar', ax=ax,
+    ylabel=unit, xlabel='Province')
+ax.set_title('Hectares of Fire')
+ax.set_ylim(0, rounded_upper_limit)
+ax.set_xticklabels([])
+stats = st.sidebar.pyplot(fig)
