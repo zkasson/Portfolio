0592cda001c7c60e9b48de04fdff9294c09e6cff
zkasson <121266165+zkasson@users.noreply.github.com>
2024-11-19 16:04:45 -0800

    Update wildfireApp.py

--- wildfireApp.py
+++ wildfireApp.py
@@ -146,7 +146,7 @@ if area_selection == 'Canadian Wildfires':
         area_final["Color"] = area_final["Stage_of_Control"].map(colors)
 
         # Create the plot
-        fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+        fig, ax = plt.subplots(1, 1)
         bars = ax.bar(
             area_final["Stage_of_Control"],
             area_final["Area"],
