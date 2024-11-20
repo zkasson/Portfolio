b38cafff1d7e5ea8a864e096a326c49ef26c53d2
zkasson <121266165+zkasson@users.noreply.github.com>
2024-11-20 14:30:34 -0800

    Update wildfireApp.py

--- wildfireApp.py
+++ wildfireApp.py
@@ -65,9 +65,6 @@ if area_selection == 'Canadian Wildfires':
     province = st.sidebar.selectbox('Select a Province', provinces)
     basemap_selection = st.sidebar.selectbox('Select a basemap', ['CartoDB.Positron', 'CartoDB.DarkMatter', 'openstreetmap','ESRI'])
 
-    # Create check box and sdf for US fires
-    conus_fires = wildfire_sdf[wildfire_sdf['Agency'] == 'conus']
-
 
     # Map different stages of control
     stage_of_control_mapping = {
@@ -87,12 +84,11 @@ if area_selection == 'Canadian Wildfires':
     area_sdf = area_sdf.pivot(index='Province', columns='Stage_of_Control', values='Hectares__Ha_').fillna(0)
     area_sdf.columns.name = None
 
-
+    # Create unit variable and engineer data to use correct unit
     unit = st.sidebar.radio(
         "Select a Unit",
         ['Hectares', 'Acres']
     )
-
     conversion_factor = 2.47105
     def correct_unit(area_sdf,unit_df):
         if unit_df == 'Hectares':  
@@ -109,6 +105,7 @@ if area_selection == 'Canadian Wildfires':
             # Handle unexpected unit cases
             raise ValueError(f"Unsupported unit: {unit_df}")
     area_final = correct_unit(area_sdf,unit)
+
 
     # Dynamically select only the columns present in the DataFrame
     available_columns = [col for col in ['Being Held', 'Out of Control', 'Prescribed', 'Under Control'] if col in area_final.columns]
@@ -222,21 +219,156 @@ else:
     st.sidebar.info('Explore Active Wildfire in the US')
     st.write(f'**US Wildfire Dashboard coming soon**')
 
-    json_file = r'https://raw.githubusercontent.com/zkasson/Portfolio/refs/heads/main/US_States.json'
+    # Dictionary to map orgin codes to state names, desired columns, Map fire types
+    origin_to_state = {
+        'US-AL': 'Alabama','US-AK': 'Alaska','US-AZ': 'Arizona','US-AR': 'Arkansas','US-CA': 'California','US-CO': 'Colorado','US-CT': 'Connecticut',
+        'US-DE': 'Delaware', 'US-FL': 'Florida', 'US-GA': 'Georgia','US-HI': 'Hawaii','US-ID': 'Idaho','US-IL': 'Illinois','US-IN': 'Indiana',
+        'US-IA': 'Iowa','US-KS': 'Kansas','US-KY': 'Kentucky','US-LA': 'Louisiana','US-ME': 'Maine','US-MD': 'Maryland','US-MA': 'Massachusetts','US-MI': 'Michigan','US-MN': 'Minnesota',
+        'US-MS': 'Mississippi','US-MO': 'Missouri','US-MT': 'Montana','US-NE': 'Nebraska','US-NV': 'Nevada','US-NH': 'New Hampshire','US-NJ': 'New Jersey','US-NM': 'New Mexico',
+        'US-NY': 'New York','US-NC': 'North Carolina','US-ND': 'North Dakota','US-OH': 'Ohio','US-OK': 'Oklahoma','US-OR': 'Oregon','US-PA': 'Pennsylvania',
+        'US-RI': 'Rhode Island','US-SC': 'South Carolina','US-SD': 'South Dakota','US-TN': 'Tennessee','US-TX': 'Texas','US-UT': 'Utah','US-VT': 'Vermont',
+        'US-VA': 'Virginia','US-WA': 'Washington','US-WV': 'West Virginia','US-WI': 'Wisconsin','US-WY': 'Wyoming','US-DC': 'District of Columbia','US-PR': 'Puerto Rico'
+    }
+    desired_columns = [
+        "OBJECTID","IncidentName","IncidentTypeCategory","DailyAcres","PercentContained","FireDiscoveryDateTime","DiscoveryAcres",
+        "POOCounty","POOState","FireCause","TotalIncidentPersonnel","ResidencesDestroyed","OtherStructuresDestroyed","Injuries"
+    ]
+    fire_type = {
+        'WF': 'Contained',
+        'RX': 'Prescribed'
+        }
 
+    json_file = r'https://raw.githubusercontent.com/zkasson/Portfolio/refs/heads/main/US_States.json'
+    item_id = "d957997ccee7408287a963600a77f61f"
 
     def read_json(url):
         prov_gdf = gpd.read_file(url)
         return prov_gdf
+    def read_fl(item_id):
+        living_atlas_item = gis.content.get(item_id)
+        feature_layer = living_atlas_item.layers[0]
+        sdf = feature_layer.query(where="1=1", out_sr=4326).sdf
+        return sdf
 
+    # Read in data
     state_gdf = read_json(json_file)
+    wildfire_sdf = read_fl(item_id)
 
-
-    st.write(state_gdf)
+    # Create dropdown for States and basemap
     states = state_gdf['State'].unique()
     state = st.sidebar.selectbox('Select a State', states)
     basemap_selection = st.sidebar.selectbox('Select a basemap', ['CartoDB.Positron', 'CartoDB.DarkMatter', 'openstreetmap','ESRI'])
 
+    # Data Engineering of wild fires
+    wildfire_sdf = wildfire_sdf[desired_columns]
+    wildfire_sdf['State'] = wildfire_sdf['POOState'].map(origin_to_state)
+    wildfire_sdf = wildfire_sdf.drop(columns=['POOState'])
+    wildfire_sdf['Type'] = wildfire_sdf['IncidentTypeCategory'].map(fire_type)
+    wildfire_sdf = wildfire_sdf.drop(columns=['IncidentTypeCategory'])
+
+    def update_type(row):
+        if row['Type'] == 'Prescribed':
+            return 'Prescribed'
+        elif pd.isna(row['PercentContained']):
+            return 'Unknown Containment'
+        elif 0 < row['PercentContained'] < 100:
+            return 'Actively Containing'
+        elif row['PercentContained'] == 0:
+            return 'Uncontained'
+        else:
+            return row['Type']
+    wildfire_sdf['Type'] = wildfire_sdf.apply(update_type, axis=1)
+
+    # Group by State and Type, then sum Acres
+    area_sdf = wildfire_sdf.groupby(['State', 'Type'])['DailyAcres'].sum().reset_index()
+    area_sdf = area_sdf.pivot(index='State', columns='Type', values='DailyAcres').fillna(0)
+    area_sdf.columns.name = None
+
+    # Create unit variable and engineer data to use correct unit
+    unit = st.sidebar.radio(
+        "Select a Unit",
+        ['Hectares', 'Acres']
+    )
+    conversion_factor = 2.47105
+    def correct_unit(area_sdf,unit_df):
+        if unit_df == 'Acres':  
+            area_final = area_sdf 
+            return area_final
+        elif unit_df == 'Hectares':
+            area_final = area_sdf
+            columns_to_convert = ['Actively Containing', 'Contained', 'Uncontained', 'Unknown Containment','Prescribed']
+            for col in columns_to_convert:
+                if col in area_final.columns:
+                    area_final[col] = area_final[col] / conversion_factor
+            return area_final
+        else:
+            # Handle unexpected unit cases
+            raise ValueError(f"Unsupported unit: {unit_df}")
+    area_final = correct_unit(area_sdf,unit)
+
+    # Dynamically select only the columns present in the DataFrame
+    available_columns = [col for col in ['Contained', 'Actively Containing', 'Prescribed', 'Uncontained','Unknown Containment'] if col in area_final.columns]
+    area_final = area_final.reset_index().melt(id_vars=['State'], 
+                                                    value_vars=available_columns,
+                                                    var_name='Type', 
+                                                    value_name='Area')
+    
+    # Filter data for the selected state
+    area_final = area_final[area_final['State'] == state]
+    no_fires_bool = area_final['Area'].dropna().empty
+
+    if area_final['Area'].dropna().empty:
+        rounded_upper_limit =5000
+    else:
+        # Calculate the upper limit for the y-axis
+        max_sh = area_final['Area'].max()  # Find the max value in the area column
+        upper_limit = max_sh + (max_sh / 10) if max_sh > 0 else 1000
+        rounded_upper_limit = round(upper_limit / 100) * 100 if upper_limit > 99 else 100
+
+    # # # Create Chart # # #
+    colors = {
+        "Actively Containing": "orange",
+        "Uncontained": "red",
+        "Contained": "green",
+        "Prescribed": "#CCCC00",
+        "Unknown Containment": "gray"  
+    }
+    area_final["Color"] = area_final["Type"].map(colors)
+
+    # Create the plot
+    fig, ax = plt.subplots(1, 1)
+    bars = ax.bar(
+        area_final["Type"],
+        area_final["Area"],
+        color=area_final["Color"]
+    )
+    # Customize the plot
+    plt.xticks(rotation=15, ha='right') 
+    ax.set_ylabel(f'Area ({unit})', fontsize=14, fontweight='bold') 
+    ax.set_xlabel('Control Stage', fontsize=14, fontweight='bold')
+    ax.set_title(f'{unit} of fire within {state} by control stage')
+    ax.set_ylim(0, rounded_upper_limit)
+
+    ax.yaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter("{x:,.0f}"))
+
+    for bar, area in zip(bars, area_final["Area"]):
+        # Calculate y position for the annotation text
+        text_y = bar.get_height() + 0.02 * rounded_upper_limit  
+        text_y = min(text_y, rounded_upper_limit)  
+        ax.text(
+            bar.get_x() + bar.get_width() / 2, # x-position 
+            text_y, # y-position 
+            f'{area:,.0f} {unit}',  
+            ha='center',  
+            va='bottom',  
+            fontsize=10  
+        )
+
+    # Display the plot
+    plt.tight_layout()
+    stats = st.sidebar.pyplot(fig, use_container_width=True)
+
+    # # # Create Map # # #
     map = leafmap.Map(
         layers_control=True,
         draw_control=False,
@@ -265,6 +397,7 @@ else:
 
 
     map_streamlit = map.to_streamlit(800, 600)
+
 
 
 
